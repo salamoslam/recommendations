26f198b7f233660701941433175f2123ac9b7894
salamoslam <kuznetsovv.np@gmail.com>
2021-10-13 17:05:05 +0300

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='recommendations system for consignment store in Moscow',
+    author='nikita kuznetsov',
+    license='MIT',
+)
