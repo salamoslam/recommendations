fdd22ffbbcb30347aab94ee9d2a6dcdce27ecc25
salamoslam <kuznetsovv.np@gmail.com>
2022-01-17 23:24:17 +0300

    подкорректировал выводы табличек

--- src/data/get_preference_matrix.py
+++ src/data/get_preference_matrix.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import datetime
 
 path_to_repo = '/Users/kuznetsovnikita'
 def get_pref_matrix(to_csv = True,
@@ -14,42 +15,50 @@ def get_pref_matrix(to_csv = True,
     import_path = path_to_repo + '/recommendations/data/raw/'
     export_path = path_to_repo + '/recommendations/data/interim'
 
-    df_heat = pd.read_csv(import_path+'heats.csv')
-    cart = pd.read_csv(import_path+'cart.csv')
+    df_heat = pd.read_csv(import_path+'heats.csv', sep = ',', converters={'product_id':str,'ym_client_id':str})
+    cart = pd.read_csv(import_path+'cart.csv', sep = ',',converters={'product_id':str})
     users = pd.read_csv(import_path+'users.csv')
     vygruz = pd.read_excel(import_path+'goods.xlsx').iloc[:,1:]
 
+    cart['product_id'] = cart['product_id'].replace('', np.nan, regex=False).astype(float)
+    vygruz.cumdate = pd.to_datetime(vygruz.cumdate, infer_datetime_format=True)
+    vygruz.loc[:, ['days_in_stock']] = (datetime.datetime.now() - vygruz.cumdate).dt.days
+    vygruz.loc[:, ['discount']] = np.where(vygruz['Цена со скидкой'].isna(), 0,
+                                           (vygruz['Цена шоурум'] - vygruz['Цена со скидкой']) / vygruz['Цена шоурум'])
+    vygruz = vygruz.loc[vygruz.id != '']
+    # vygruz.id = vygruz.id.replace(' ','',regex=True)
 
     vygruz = vygruz.loc[~vygruz.id.isnull()]
     vygruz = vygruz[vygruz['id'].str.isnumeric()]
-    vygruz.loc[:,['id_s']] = vygruz.id.astype(str)
-    # print(vygruz.info())
-    vygruz['id'] = vygruz.id.replace('',np.nan, regex=False).astype(int)
-    cart = cart.dropna(subset = ['product_id'])
-    cart.loc[:,['product_id']] = cart.product_id.astype(int)
+    vygruz['id'] = vygruz.id.replace('', np.nan, regex=False).astype(int)
 
-    products = df_heat.loc[(df_heat.product_id != '0')&(df_heat.product_id != '16777215')] #выкидываю технические неполадки
+    ## потоварная статистика
+    df_heat['product_id'] = df_heat['product_id'].str.rstrip('.0')
+    products = df_heat.loc[
+        (~(df_heat.product_id.isin(['0', '']))) & (df_heat.product_id != '16777215')]  # выкидываю технические неполадки
 
-    df_heat['product_id'] = df_heat['product_id'].astype(str).str.rstrip('.0')
 
-    # задаются параметры для заполнения матрицы предпочтений
+    vygruz.loc[:, ['id_s']] = vygruz.id.astype(str)
 
+    # теперь аналогично посмотрим на добавления в корзину/вишлисты
+    vygruz['id'] = vygruz.id.replace('', np.nan, regex=False).astype(int)
+    cart = cart.dropna(subset=['product_id'])
+    cart.loc[:, ['product_id']] = cart.product_id.astype(int)
 
 
 
     # достаю списки товаров из корзин/вишлистов/заказов каждого клиента
-    cart_user_prod = cart.loc[(~cart.ym_client_id.isin([None,0,'','0','null']))
-                              &(cart.type == 'W')].groupby('ym_client_id')['product_id'].apply(list).to_frame()
-    wish_user_prod = cart.loc[(~cart.ym_client_id.isin([None,0,'','0','null']))
-                              &(cart.type == 'C')].groupby('ym_client_id')['product_id'].apply(list).to_frame()
+    cart_user_prod = cart.loc[(~cart.ym_client_id.isin([None, 0, '', '0', 'null']))
+                              & (cart.type == 'W')].groupby('ym_client_id')['product_id'].apply(list).to_frame()
+    wish_user_prod = cart.loc[(~cart.ym_client_id.isin([None, 0, '', '0', 'null']))
+                              & (cart.type == 'C')].groupby('ym_client_id')['product_id'].apply(list).to_frame()
     # засовываю в единый фрейм с учетом непересечений
     cart_wish_user_prod = cart_user_prod.join(wish_user_prod,
-                                              how = 'outer',
-                                              on ='ym_client_id',
-                                              lsuffix = '_cart',
-                                              rsuffix = '_wish').drop_duplicates('ym_client_id').reset_index().drop(columns = 'index')
-
-
+                                              how='outer',
+                                              on='ym_client_id',
+                                              lsuffix='_cart',
+                                              rsuffix='_wish').drop_duplicates('ym_client_id').reset_index().drop(
+        columns='index')
 
 
 
@@ -58,12 +67,10 @@ def get_pref_matrix(to_csv = True,
     products = products.loc[products.product_id != 'NaN']
 
     # делаю список со всеми возможными вещами, обрезать по наличию буду потом
-    all_items = np.concatenate([products.product_id.unique(), cart.product_id.astype(str).unique()])
+    all_items = np.unique(np.concatenate([products.product_id, cart.product_id.astype(str)]))
     product_id_cols = [str(i) for i in all_items]
     product_id_cols.extend(['product_id_wish', 'product_id_cart', 'product_id', 'ym_client_id'])
     product_id_cols.reverse()
-
-
 
     user_product_heat = products.groupby('ym_client_id')['product_id'].apply(list).reset_index()
     user_product_heat = user_product_heat.merge(cart_wish_user_prod, how='outer', on='ym_client_id')
@@ -85,13 +92,11 @@ def get_pref_matrix(to_csv = True,
 
     user_product_heat.drop(columns=['product_id', 'product_id_cart', 'product_id_wish'], inplace=True)
 
-    # после этого, по сути, надо дропнуть вещи, которые уже архивные
-
     combinations = {
-        1:['categ'],
-        2:['brand'],
-        3:['categ','brand'],
-        4:['brand','Группа категорий']
+        #     1:['categ'],
+        #     2:['brand'],
+        #     3:['categ','brand'],
+        4: ['brand', 'Группа категорий']
     }
     dict_combs = {}
     for num, comb in combinations.items():
@@ -105,7 +110,19 @@ def get_pref_matrix(to_csv = True,
         dict_combs[num] = dict_combs[num].join(stock)
         dict_combs[num] = dict_combs[num].loc[~((dict_combs[num].id_s.isna()) | (dict_combs[num].id_s == 1))]
 
+        dict_combs[num].loc['user_total'] = dict_combs[num].drop(columns=['id_s']).sum(axis=0)
+        dict_combs[num].loc[:, ['item_total']] = dict_combs[num].drop(columns=['id_s']).sum(axis=1)
+    #     dict_combs[tuple(comb)].append(dict_combs[tuple(comb)].sum(numeric_only=True), ignore_index=True)
+
     # print(dict_combs[4])
     if to_csv:
         dict_combs[4].to_csv(os.path.join(export_path,'user_group_info.csv'))
     return dict_combs[4]
+
+
+
+
+# чекер на заполненность
+# dict_combs[4].drop(columns=['id_s', 'item_total'], index=['user_total']).values.mean()
+
+
--- src/data/get_users_info.py
+++ src/data/get_users_info.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import datetime
 
 path_to_repo = '/Users/kuznetsovnikita'
 def get_users_info(to_csv = True, path_to_repo='/Users/kuznetsovnikita'):
@@ -10,21 +11,34 @@ def get_users_info(to_csv = True, path_to_repo='/Users/kuznetsovnikita'):
     import_path = path_to_repo + '/recommendations/data/raw/'
     export_path = path_to_repo + '/recommendations/data/interim'
 
-    df_heat = pd.read_csv(import_path+'heats.csv')
-    cart = pd.read_csv(import_path+'cart.csv')
+    df_heat = pd.read_csv(import_path+'heats.csv', sep = ',', converters={'product_id':str,'ym_client_id':str})
+    cart = pd.read_csv(import_path+'cart.csv', sep = ',',converters={'product_id':str})
     users = pd.read_csv(import_path+'users.csv')
     vygruz = pd.read_excel(import_path+'goods.xlsx').iloc[:,1:]
 
+    cart['product_id'] = cart['product_id'].replace('', np.nan, regex=False).astype(float)
+    vygruz.cumdate = pd.to_datetime(vygruz.cumdate, infer_datetime_format=True)
+    vygruz.loc[:, ['days_in_stock']] = (datetime.datetime.now() - vygruz.cumdate).dt.days
+    vygruz.loc[:, ['discount']] = np.where(vygruz['Цена со скидкой'].isna(), 0,
+                                           (vygruz['Цена шоурум'] - vygruz['Цена со скидкой']) / vygruz['Цена шоурум'])
+    vygruz = vygruz.loc[vygruz.id != '']
+    # vygruz.id = vygruz.id.replace(' ','',regex=True)
+
     vygruz = vygruz.loc[~vygruz.id.isnull()]
     vygruz = vygruz[vygruz['id'].str.isnumeric()]
-    # print(vygruz.info())
-    vygruz['id'] = vygruz.id.replace('',np.nan, regex=False).astype(int)
-    cart = cart.dropna(subset = ['product_id'])
-    cart.loc[:,['product_id']] = cart.product_id.astype(int)
+    vygruz['id'] = vygruz.id.replace('', np.nan, regex=False).astype(int)
+
+    ## потоварная статистика
+    df_heat['product_id'] = df_heat['product_id'].str.rstrip('.0')
+    vygruz.loc[:, ['id_s']] = vygruz.id.astype(str)
+
+    # теперь аналогично посмотрим на добавления в корзину/вишлисты
+    vygruz['id'] = vygruz.id.replace('', np.nan, regex=False).astype(int)
+    cart = cart.dropna(subset=['product_id'])
+    cart.loc[:, ['product_id']] = cart.product_id.astype(int)
 
     cart_full = cart.merge(vygruz.loc[:,['id','categ']], how='left', left_on='product_id', right_on='id')
 
-    df_heat['product_id'] = df_heat['product_id'].astype(str).str.rstrip('.0')
 
     # ready table one
     # по строчкам юзеры, по столбцам инфа по ним (даты визитов, число просмотров и тд)
@@ -32,24 +46,33 @@ def get_users_info(to_csv = True, path_to_repo='/Users/kuznetsovnikita'):
     users_info.index = df_heat.ym_client_id.unique()
 
     # всего переходов по страницам
-    users_info['views'] = df_heat.groupby('ym_client_id').agg({'_id':'count'})
-    #список просмотренных товаров
-    users_info['products'] = df_heat.loc[~df_heat.product_id.isin([0,'0'])].groupby('ym_client_id').agg({'product_id':[list,'count']})['product_id']['list']
-    #кол-во просмотренных товаров (возможны повторные просмотры тех же товаров)
-    users_info['products_quan'] = df_heat.loc[~df_heat.product_id.isin([0,'0'])].groupby('ym_client_id').agg({'product_id':[list,'count']})['product_id']['count']
-    #добавления в корзину
-    users_info['carts'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null','','0']))&(cart_full.type=='C')
-                 ].groupby('ym_client_id').agg({'product_id':[list,'count']})['product_id']['list']
-    #кол-во добавлений в корзину
-    users_info['carts_quan'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null','','0']))&(cart_full.type=='C')
-                 ].groupby('ym_client_id').agg({'product_id':[list,'count']})['product_id']['count']
+    users_info['views'] = df_heat.groupby('ym_client_id').agg({'_id': 'count'})
+    # список просмотренных товаров
+    users_info['products'] = \
+    df_heat.loc[~df_heat.product_id.isin([0, '0', ''])].groupby('ym_client_id').agg({'product_id': [list, 'count']})[
+        'product_id']['list']
+    # кол-во просмотренных товаров (возможны повторные просмотры тех же товаров)
+    users_info['products_quan'] = \
+    df_heat.loc[~df_heat.product_id.isin([0, '0', ''])].groupby('ym_client_id').agg({'product_id': [list, 'count']})[
+        'product_id']['count']
+    # добавления в корзину
+    users_info['carts'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null', '', '0'])) & (cart_full.type == 'C')
+                                        ].groupby('ym_client_id').agg({'product_id': [list, 'count']})['product_id'][
+        'list']
+    # кол-во добавлений в корзину
+    users_info['carts_quan'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null', '', '0'])) & (cart_full.type == 'C')
+                                             ].groupby('ym_client_id').agg({'product_id': [list, 'count']})[
+        'product_id']['count']
 
-    #добавления в вишлист
-    users_info['wish'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null','','0']))&(cart_full.type=='W')
-                 ].groupby('ym_client_id').agg({'product_id':[list,'count']})['product_id']['list']
-    #кол-во добавлений в вишлист
-    users_info['wish_quan'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null','','0']))&(cart_full.type=='W')
-                 ].groupby('ym_client_id').agg({'product_id':[list,'count']})['product_id']['count']
+    # добавления в вишлист
+    users_info['wish'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null', '', '0'])) & (cart_full.type == 'W')
+                                       ].groupby('ym_client_id').agg({'product_id': [list, 'count']})['product_id'][
+        'list']
+    # кол-во добавлений в вишлист
+    users_info['wish_quan'] = cart_full.loc[(~cart_full.ym_client_id.isin(['null', '', '0'])) & (cart_full.type == 'W')
+                                            ].groupby('ym_client_id').agg({'product_id': [list, 'count']})[
+        'product_id']['count']
+
     if to_csv:
         users_info.to_csv(os.path.join(export_path,r'users_info.csv'))
-    return users_info
+    return users_info, cart_full
